3c98ffe0444980ac9e238bf2d58cf96643b98aa7
Monty Dimkpa <cmdimkpa@gmail.com>
2020-08-26 19:03:35 +0100

    Update make-data.py

--- data/generators/make-data.py
+++ data/generators/make-data.py
@@ -6,6 +6,7 @@
 from random import random
 from hashlib import md5
 import os
+import pymssql
 
 user_names = {
     "first" : [
@@ -78,7 +79,7 @@ product_names = {
     ]
 }
 
-global users, num_users, products, num_products, transactions, HOME, slash
+global users, num_users, products, num_products, transactions, HOME, slash, conn, cursor, create_table_queries, myHeaders, insert_table_queries
 
 users = []
 num_users = 400
@@ -91,6 +92,27 @@ if "\\" in HOME:
 else:
     slash = "/"
 HOME+=slash
+
+create_table_queries = {
+    "users": "CREATE TABLE Users (user_id VARCHAR(10) PRIMARY KEY, username VARCHAR(100) NOT NULL, age INTEGER NOT NULL);",
+    "products" : "CREATE TABLE Products (product_id VARCHAR(20) PRIMARY KEY, productname VARCHAR(200) NOT NULL, price FLOAT(24) NOT NULL);",
+    "transactions": "CREATE TABLE Transactions (purchase_date DATETIME NOT NULL, customer_id VARCHAR(10) NOT NULL, product_id VARCHAR(20) NOT NULL, price FLOAT(24) NOT NULL);"
+}
+
+insert_table_queries = {
+    "users": "INSERT INTO Users VALUES ('%s', '%s', %s);",
+    "products" : "INSERT INTO Products VALUES ('%s', '%s', %s);",
+    "transactions": "INSERT INTO Transactions VALUES ('%s', '%s', '%s', %s);"
+}
+
+myHeaders = {
+    "users" : ["user_id", "username", "age"],
+    "products" : ["product_id", "productname", "price"],
+    "transactions" : ["purchase_date", "customer_id", "product_id", "price"]
+}
+
+conn = pymssql.connect(user="SA",password="Photon86$",host="localhost",database="PrestoDBTest")
+cursor = conn.cursor()
 
 def random_from(array):
     ptr = int(random()*len(array))
@@ -120,6 +142,17 @@ def write2csv(collection, filename):
         h.close()
     return file_
 
+
+def write2table(collection, table):
+    cursor.execute("DROP TABLE %s" % table.title()); conn.commit()
+    cursor.execute(create_table_queries[table]); conn.commit()
+    for entry in collection:
+        sql = insert_table_queries[table] % tuple(
+            [entry[header] for header in myHeaders[table]])
+        cursor.execute(sql)
+        conn.commit()
+    return "wrote %s lines to table: %s" % (len(collection), table)
+
 while len(users) < num_users:
     username = "%s %s" % (random_from(user_names["first"]), random_from(user_names["last"]))
     if not [x for x in users if x["username"] == username]:
@@ -147,12 +180,17 @@ for day in days:
         selection = random_array_from(products, int(80 + random()*20))
         for product in selection:
             transactions.append({
-                    "date" : date,
+                    "purchase_date" : date,
                     "customer_id" : customer["user_id"],
                     "product_id" : product["product_id"],
                     "price" : product["price"]
                 })
 
 print(write2csv(users, "users"))
+print(write2table(users, "users"))
+
 print(write2csv(products, "products"))
+print(write2table(products, "products"))
+
 print(write2csv(transactions, "transactions"))
+print(write2table(transactions, "transactions"))
