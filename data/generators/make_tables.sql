793cd4d0b0d0365da102a3464869fdbfa1767362
Monty Dimkpa <cmdimkpa@gmail.com>
2020-08-27 08:50:17 +0100

    Create make_tables.sql

---
+++ data/generators/make_tables.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Users (user_id VARCHAR(10) PRIMARY KEY, username VARCHAR(100) NOT NULL, age INTEGER NOT NULL);
+CREATE TABLE Products (product_id VARCHAR(20) PRIMARY KEY, productname VARCHAR(200) NOT NULL, price FLOAT(24) NOT NULL);
+CREATE TABLE Transactions (purchase_date DATETIME NOT NULL, customer_id VARCHAR(10) NOT NULL, product_id VARCHAR(20) NOT NULL, price FLOAT(24) NOT NULL);
