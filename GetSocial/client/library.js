00a9888e963d5b0d699ccd4880d2a2197bcaf6e9
Monty Dimkpa <cmdimkpa@gmail.com>
2020-09-01 22:41:20 +0100

    Update library.js

--- GetSocial/client/library.js
+++ GetSocial/client/library.js
@@ -107,7 +107,7 @@ const checkUserAuthenticated = async () => {
 
 const LoginRegisterForm = async () => {
     // waitscreen
-    $('#console').html(`<img src="https://cmdimkpa.github.io/GetSocial/client/waitscreen.gif" alt="alien-detected" class="center"><div class="center"><h6 style="text-align: center;">© Monty Dimkpa</h6></div>`)
+    $('#console').html(`<img src="https://cmdimkpa.github.io/GetSocial/client/waitscreen.gif" alt="alien-detected" class="center"><div class="center"><h6 style="text-align: center;">© Monty Dimkpa</h6></div><p class="blink_me">Please login or register below</p>`)
 }
 
 const globalUpdate = async () => {
